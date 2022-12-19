50cd0d4f1c4aa1c348ebe25a736a809210941007
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-12-19 11:47:26 -0300

    update nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,12 +2,20 @@ import React from 'react'
 import './Nav.css'
 
 const Nav = () => {
-  return (
-    <div className='nav-container'>
-        <img className='nav-logo' src='' alt='netflix logo'></img>
-        <img className='nav-avatar' alt='netflix avatar'></img>
-    </div>
-  )
+    return (
+        <div className='nav-container'>
+            <img
+                className="nav-logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+                alt="Netflix"
+            ></img>
+            <img
+                className="nav-avatar"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPsVAeFlYeYOEUzb3TV1ML91_LPkkFML5lRQcMdr9nQu2CqO-WzT-RLmkM5_cOKvkaBkI&usqp=CAU"
+                alt="Pasquadev"
+            ></img>
+        </div>
+    )
 }
 
 export default Nav;
