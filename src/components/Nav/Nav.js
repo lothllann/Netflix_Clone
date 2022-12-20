b37cd6f230b5ed8085e618617b214e67fc99cbd5
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-12-19 21:00:45 -0300

    toques finais

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,8 +2,16 @@ import React from 'react'
 import './Nav.css'
 
 const Nav = () => {
+    const [show, setShow] = React.useState(false);
+
+    React.useEffect(() => {
+        window.addEventListener('scroll', () => {
+            setShow(window.scrollY > 80)
+        });
+    }, []);
+
     return (
-        <div className='nav-container'>
+        <div className={`nav-container ${show && 'nav-container-black'}`}>
             <img
                 className="nav-logo"
                 src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,10 +1,28 @@
+import movieTrailer from 'movie-trailer';
 import React from 'react'
+import ReactPlayer from 'react-player';
 import { getMovies } from '../../api'
 import './Row.css'
 
 const imageHost = "https://image.tmdb.org/t/p/original";
 const Row = ({ title, path, isLarge }) => {
-    const [movies, setMovies] = React.useState([])
+    const [movies, setMovies] = React.useState([]);
+    const [trailerUrl, setTrailerUrl] = React.useState('');
+
+    const handleOnClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('')
+        } else {
+            movieTrailer(movie?.title || movie?.name || movie?.original_name || '')
+                .then((url) => {
+                    setTrailerUrl(url)
+                })
+                .catch((error) => {
+                    console.log(error)
+                });
+        }
+    };
+
     const fetchMovies = async (_path) => {
         try {
             const data = await getMovies(_path);
@@ -24,6 +42,7 @@ const Row = ({ title, path, isLarge }) => {
                     return (
                         <img
                             className={`movie-card ${isLarge && 'movie-card-large'}`}
+                            onClick={() => handleOnClick(movie)}
                             key={movie.id}
                             src={`${imageHost}${isLarge ? movie.backdrop_path : movie.poster_path}`}
                             alt={movie.title}>
@@ -31,6 +50,7 @@ const Row = ({ title, path, isLarge }) => {
                     )
                 })}
             </div>
+            {trailerUrl && <ReactPlayer url={trailerUrl} playing={true} />}
         </div>
     )
 }
